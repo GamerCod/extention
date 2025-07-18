cd57bbfd5f2409f44ac11a32a2ba1a98509b5403
GamerCod <oyedeleemmanuel2019@gmail.com>
2025-07-18 21:30:09 +0100

    updated

--- extenint.js
+++ extenint.js
@@ -1,7 +1,7 @@
-// Select ALL toggle buttons
+   
 const toggleButtons = document.querySelectorAll('.oval');
 
-// Loop through each one and attach event
+
 toggleButtons.forEach(button => {
   button.addEventListener('click', () => {
     button.classList.toggle('active');
@@ -17,17 +17,18 @@ darkToggle.addEventListener('click', () => {
 
 
 const filterButtons = document.querySelectorAll('.filter-bttn');
+const extensionGrid = document.querySelector('.extension-grid');
 
 filterButtons.forEach(button => {
   button.addEventListener('click', () => {
     const filterType = button.id;
     console.log(`You clicked: ${filterType}`);
 
-    // Optional: add an 'active' style
+    
     filterButtons.forEach(btn => btn.classList.remove('active'));
     button.classList.add('active');
-
-    // You can now filter cards or content here based on filterType
+    
+ 
   });
 });
 
